b82a5175c6c79bcedad3a1c8c5f199dbf071a540
k.f.gamehacker <k.f.gamehacker@gmail.com>
2019-05-04 13:58:51 +0800

    init the custom router

--- routes.js
+++ routes.js
@@ -1,8 +1,8 @@
-const routes = require('next-routes');
+const routes = require('next-routes')();
 
-module.exports = routes()
-.add('/projects/create','projects/create')
-.add('/projects/:address','projects/detail')
-.add('/projects/:address/payments/create','projects/payments/create');
+routes
+    .add('/projects/create','projects/create')
+    .add('/projects/:address','projects/detail')
+    .add('/projects/:address/payments/create','projects/payments/create');
 
 module.exports = routes;
